024949cd39c899ec6f261f8b1a1f16383d82a7fa
sp8rks <34198848+sp8rks@users.noreply.github.com>
2022-01-31 16:19:45 -0700

    updated function example 2022

--- module_examples/13_function_example.py
+++ module_examples/13_function_example.py
@@ -1,24 +1,55 @@
-def f2c (temp = 15):
-    new_temp = (temp-32)*5/9
-    return new_temp
-
-t1 = 75 #23C
-t2 = 50 #10C
-
-tempC = f2c()
+def name_joiner(first_name, last_name):
+    full_name = first_name + " " + last_name
+    print(full_name)
+    return first_name, last_name, full_name
 
 
+the_first_name, the_last_name, the_full_name = name_joiner("taylor", "sparks")
 
-def try_recursion(i):
-    if i>0:
-        result = i + try_recursion(i-1)
-        print(result)
+
+def add_em_up(*items):
+    summation = 0
+    for catfish in items:
+        summation = summation + catfish
+    return summation
+
+
+list_of_nums = [5, 6, 8, 10, 11]
+
+total_sum = add_em_up(*list_of_nums)
+
+
+name_dict = {"formula": "ABO3"}
+
+
+def formula_printer(**kwargs):
+    print(kwargs["formula"])
+
+
+formula_printer(**name_dict)
+
+
+# %%
+def my_recursion(i):
+    """
+    prints value of integer and then reduces its by 1 each time
+    only prints until i <1
+    Parameters
+    ----------
+    i : int
+        value to be printed and iterated.
+
+    Returns
+    -------
+    None.
+
+    """
+    if i < 1:
+        return None
     else:
-        result = 0
-    return result
+        print(i)
+    i = i - 1
+    my_recursion(i)
 
-try_recursion(5)
 
-
-list_of_words = ['caterpillar', 'puppy', 'bat', 'monster']
-list_of_words.sort(key=lambda x: len(set(list(x))))
+my_recursion(5)
